5eb3487a243d3be2068976584bdca3583beea562
Shivam <shivam.upadhyay@armantecsystems.com>
2025-04-19 15:28:43 +0530

    Initial commit for CI/CD pipeline project

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("java program run and build using jenkins..");
+        System.out.println(new Date());
+    }
+}
