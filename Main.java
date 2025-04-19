7f859ea504972eba070d1914f07ff70c3bd21b1a
Shivam <shivam.upadhyay@armantecsystems.com>
2025-04-19 15:52:21 +0530

    Refactored Main.java: added error handling

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("java program run and build using jenkins..");
+        for(int i=0; i<=10; i++)
+        {
+            System.out.println("Number is "+i);
+        }
         System.out.println(new Date());
     }
 }
